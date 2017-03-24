50ecf03a7ef884885bf249e995dfe34cdeff5b5b
panopticons <thepanopticons@gmail.com>
2017-03-23 22:57:10 -0700

    update

--- canvas/ViewController.swift
+++ canvas/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
   var trayDownOffset: CGFloat!
   var trayUp: CGPoint!
   var trayDown: CGPoint!
+  var newlyCreatedFace: UIImageView!
+  var newlyCreatedFaceOriginalCenter: CGPoint!
   
   @IBOutlet weak var trayView: UIView!
   
@@ -21,7 +23,7 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
     
-    trayDownOffset = 160
+    trayDownOffset = 0
     trayUp = trayView.center
     trayDown = CGPoint(x: trayView.center.x , y: trayView.center.y + trayDownOffset)
   }
@@ -34,7 +36,7 @@ class ViewController: UIViewController {
   @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
     
     let translation = sender.translation(in: view)
-    var velocity = sender.velocity(in: view)
+    let velocity = sender.velocity(in: view)
     
     if sender.state == .began {
       trayOriginalCenter = trayView.center
@@ -43,9 +45,41 @@ class ViewController: UIViewController {
       trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
     }
     else if sender.state == .ended {
+      if velocity.y > 0 {
+        UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                       animations: { () -> Void in
+                        self.trayView.center = self.trayDown
+        }, completion: nil)
+      }
+      else
+      {
+        UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                       animations: { () -> Void in
+                        self.trayView.center = self.trayUp
+        }, completion: nil)
+      }
+    }
+  }
+  
+  @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+    let translation = sender.translation(in: view)
+    
+    if sender.state == .began {
+      
+      var imageView = sender.view as! UIImageView
+      newlyCreatedFace = UIImageView(image: imageView.image)
+      view.addSubview(newlyCreatedFace)
+      newlyCreatedFace.center = imageView.center
+      newlyCreatedFace.center.y += trayView.frame.origin.y
+      newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+    }
+    else if sender.state == .changed {
+      newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+    }
+    else if sender.state == .ended {
       
     }
   }
-
+  
+  
 }
-
