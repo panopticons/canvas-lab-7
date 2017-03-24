ec8926e0bb3ec51430fb9c4470ff6c63b85f8537
panopticons <thepanopticons@gmail.com>
2017-03-23 19:37:58 -0700

    first commit

--- canvas/ViewController.swift
+++ canvas/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  var trayOriginalCenter: CGPoint!
+  var trayDownOffset: CGFloat!
+  var trayUp: CGPoint!
+  var trayDown: CGPoint!
+  
+  @IBOutlet weak var trayView: UIView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    trayDownOffset = 160
+    trayUp = trayView.center
+    trayDown = CGPoint(x: trayView.center.x , y: trayView.center.y + trayDownOffset)
   }
 
   override func didReceiveMemoryWarning() {
@@ -20,6 +31,21 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
   }
 
+  @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+    
+    let translation = sender.translation(in: view)
+    var velocity = sender.velocity(in: view)
+    
+    if sender.state == .began {
+      trayOriginalCenter = trayView.center
+    }
+    else if sender.state == .changed {
+      trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+    }
+    else if sender.state == .ended {
+      
+    }
+  }
 
 }
 
